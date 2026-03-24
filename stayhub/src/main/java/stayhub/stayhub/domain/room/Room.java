64827d14579ded2fb5f4686b0c96fecbaab148cd
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2026-03-24 15:32:59 +0900

    refactor: Room id 타입을 String에서 Long으로 변경 #1
    - JPA 사용에 맞게 id 타입을 Long으로 수정

--- stayhub/src/main/java/stayhub/stayhub/domain/room/Room.java
+++ stayhub/src/main/java/stayhub/stayhub/domain/room/Room.java
@@ -11,7 +11,7 @@ public class Room {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     private String roomNumber; // 101, 102
 
