abbfd8e05a4a7d337469ab12cccca1c04b386676
Hyejeong Yoon <108658882+hjyoon99@users.noreply.github.com>
2026-03-25 21:19:45 +0900

    test: 예약 동시성 테스트 코드 추가 #5

--- stayhub/src/main/java/stayhub/domain/reservation/dto/ReservationRequestDto.java
+++ stayhub/src/main/java/stayhub/domain/reservation/dto/ReservationRequestDto.java
@@ -1,10 +1,12 @@
 package stayhub.domain.reservation.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
+@AllArgsConstructor
 public class ReservationRequestDto {
     private Long userId;
     private Long roomId;
